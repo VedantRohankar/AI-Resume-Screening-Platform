936b4327c682ecaba773a7c14fcb06ea5129387b
vedant <vedantrohankar1@gmail.com>
2026-08-29 22:22:55 +0530

    Set up frontend authentication foundation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-
+import {BrowserRouter, Router, Route, Routes} from 'react-router-dom';
 function App() {
   return (
-    <div className='min-h-screen bg-gray-950 text-white flex items-center justify-center'>
-      <h1 className='text-5xl font-bold'>
-        HireAI
-      </h1>
-    </div>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<h1>HireAI Home</h1>}/>
+       <Route path='/login' element={<h1>Login</h1>}/>
+        <Route path='/register' element={<h1>Register</h1>}/>
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
